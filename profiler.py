e52da6e78aeedcc5574f886e26c343d0eac96184
บุรัสกร แซ่ลิ้ม <buratsakorn.sa@mail.wu.ac.th>
2026-08-31 10:07:01 +0000

    Completed Lab 1: System Profiling

--- profiler.py
+++ profiler.py
@@ -1,10 +1,11 @@
 # ==========================================
 # OS-Lab 01: System Profiler
-# Student ID: [Enter your ID here]
+# Student ID: 67117655
 # ==========================================
 import os
 import platform
 import psutil
 
-# TODO: Write your system profiler code here 
-# Follow the instructions in the Lab manual.
+print(f"OS Name: {platform.system()} {platform.release()}")
+print(f"Number of CPU Cores: {psutil.cpu_count(logical=True)}")
+print(f"Total RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB")
--- stress_test.py
+++ stress_test.py
@@ -1,8 +1,8 @@
 # ==========================================
 # OS-Lab 01: CPU Stress Test
-# Student ID: [Enter your ID here]
+# Student ID: 67117655
 # ==========================================
-import time
-
-# TODO: Write the intensive computation loop here
-# Follow the instructions in the Lab manual.
+import math
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000) # Intensive calculation
